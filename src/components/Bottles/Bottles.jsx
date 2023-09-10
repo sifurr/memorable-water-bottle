b9e7d79bb4c461ffbf3601c574f076d3c638d881
Md Saifur Rahman <saifurrshishir@gmail.com>
2023-09-10 20:08:46 +0600

    purchse button added with event handler

--- src/components/Bottles/Bottles.jsx
+++ src/components/Bottles/Bottles.jsx
@@ -1,18 +1,34 @@
 import { useEffect } from "react";
 import { useState } from "react";
+import "./Bottles.css";
+import Bottle from "../Bottle/Bottle";
 
 const Bottles = () => {
-    const [bottles, setBottles] = useState([]);
-    useEffect( ()=>{
-        fetch('bottles.json')
-        .then(res => res.json())
-        .then(data => setBottles(data))
-    }, [])
-    return (
-        <div>
-            <h2>Bottles: {bottles.length}</h2>
-        </div>
-    );
+  const [bottles, setBottles] = useState([]);
+  useEffect(() => {
+    fetch("bottles.json")
+      .then((res) => res.json())
+      .then((data) => setBottles(data));
+  }, []);
+
+  const handleAddToCart = bottle =>{
+    console.log("bottle had been added");
+    console.log(bottle);
+  }
+  return (
+    <div>
+      <h2>Bottles: {bottles.length}</h2>
+      <div className="bottles-container">        
+          {bottles.map((bottle) => (
+            <Bottle 
+            key={bottle.id} 
+            bottle={bottle}
+            handleAddToCart={handleAddToCart}
+            ></Bottle>
+          ))}       
+      </div>
+    </div>
+  );
 };
 
 export default Bottles;
