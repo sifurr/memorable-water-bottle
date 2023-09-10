7cf933474df4b2e887de5f71b8a620af4a996a5b
Md Saifur Rahman <saifurrshishir@gmail.com>
2023-09-10 19:43:57 +0600

    bottles imported

--- src/App.jsx
+++ src/App.jsx
@@ -1,12 +1,15 @@
 
 import './App.css'
+import Bottles from './components/Bottles/Bottles'
+import Header from './components/Header/Header'
 
 function App() {
 
 
   return (
     <>
-     <h2>Memorable Water Bottle</h2>
+     <Header></Header>
+     <Bottles></Bottles>
     </>
   )
 }
